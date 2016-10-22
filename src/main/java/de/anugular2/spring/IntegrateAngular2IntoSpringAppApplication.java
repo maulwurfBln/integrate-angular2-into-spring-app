92c913fdd15bfebfce45a2ac9d252f396d4bcb4c
Pierre Brichmann <pierre.brichmann@gmx.de>
2016-10-22 12:38:09 +0200

    init
    add spring boot

---
+++ src/main/java/de/anugular2/spring/IntegrateAngular2IntoSpringAppApplication.java
@@ -0,0 +1,12 @@
+package de.anugular2.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrateAngular2IntoSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrateAngular2IntoSpringAppApplication.class, args);
+	}
+}
---
+++ src/test/java/de/anugular2/spring/IntegrateAngular2IntoSpringAppApplicationTests.java
@@ -0,0 +1,16 @@
+package de.anugular2.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegrateAngular2IntoSpringAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
